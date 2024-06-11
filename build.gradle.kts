7e50afeaebfbe0f709c2697d791acdd84861e463
Timmermans <gustimmer@gmail.com>
2024-06-11 16:39:17 -0300

    Setup project dependencies
    - Koin DI framework
    - Hibernate with HikariCP
    - MySQL with Docker
    - JUnit5
    - Logback
    - Flyway for migration control

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("org.flywaydb.flyway") version "10.14.0"
+
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.24"
+}
+
+group = "com.timmermans"
+version = "1.0-SNAPSHOT"
+
+val koinVersion = "3.5.0"
+
+repositories {
+    mavenCentral()
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.flywaydb:flyway-mysql:10.14.0")
+    }
+}
+
+dependencies {
+    implementation("io.insert-koin:koin-core:$koinVersion")
+
+    implementation("org.hibernate:hibernate-core:5.6.15.Final")
+    implementation("org.hibernate:hibernate-hikaricp:5.6.15.Final")
+
+    // Required by Hibernate
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
+
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
+
+    // Required for flyway
+    implementation("mysql:mysql-connector-java:8.0.28")
+
+    testImplementation("io.insert-koin:koin-test-junit5:$koinVersion")
+    // For Parameterized tests
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveBaseName.set("app")
+    archiveVersion.set("")
+    archiveClassifier.set("")
+
+    manifest {
+        attributes["Main-Class"] = "com.timmermans.MainKt"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+flyway {
+    url = "jdbc:mysql://localhost:3306/dev"
+    user = "root"
+    password = "root"
+    locations = arrayOf("filesystem:src/main/resources/db/migrations")
+    cleanDisabled = false
+}
